d5560723b2dae6b610265d09ace3f0574a410d02
littlezayn777-beep <littlezayn777@gmail.com>
2026-09-14 16:08:24 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -28,17 +28,3 @@ self.addEventListener('activate', event => {
     ).then(() => self.clients.claim())
   );
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => {
-          cache.put(event.request, copy);
-        });
-        return response;
-      })
-.catch(() => caches.match(event.request).then(r => r || new Response('Offline', {status: 503})))
-  );
-});
